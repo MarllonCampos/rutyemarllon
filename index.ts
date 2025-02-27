8db6701aa95ba7b51cf3e55ca6e2f2ac7b2665e3
Ruty Ribeiro <rribeiropena@gmail.com>
2025-02-26 22:56:57 -0300

    Adicionando arquivos para brimeira rota

--- index.ts
+++ index.ts
@@ -4,3 +4,14 @@ import { configDotenv } from 'dotenv';
 
 configDotenv();
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_, res: Response) => {
+    res.send('Server Working Just Fine');
+  });
+
+app.listen(process.env.PORT || 3000, () => {
+console.log(`🔥 Server Running on Port: ${process.env.PORT} 🔥`);
+});
