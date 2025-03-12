3a6c6787ba00dc70b9965182bc9a0ea33901e95e
Ruty Ribeiro <rribeiropena@gmail.com>
2025-03-11 23:11:07 -0300

    adicionando rotas para reserva de item

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express';
 
-import { index, findItems, findReserved } from '../controller/items'
+import { index, findItems, findReserved, reserve } from '../controller/items'
 
 const items = Router();
 
 items.get('/', index)
 items.post('/', findItems)
 items.post('/:peopleId', findReserved)
+items.patch('/reserve', reserve)
 
 export { items }
