95ad8f714cf17a598933231434ab4889add5ce93
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2025-04-01 19:45:26 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,39 @@
 import express, { Response, Router } from 'express';
-import cors from 'cors';
+import cors, { CorsOptions, CorsOptionsDelegate } from 'cors';
 import { configDotenv } from 'dotenv';
-import { items } from './routes/items'
-import { people } from './routes/people'
+import { items } from './routes/items';
+import { people } from './routes/people';
 
 configDotenv();
+const isProduction = process.env.NODE_ENV === 'production';
+const corsOptions: CorsOptions | CorsOptionsDelegate = {
+  origin: isProduction
+    ? ['https://dev.marlloneruty.com.br', 'https://marlloneruty.com.br']
+    : (origin, callback) => {
+        if (!origin || origin.startsWith('http://localhost') || origin.startsWith('http://127.0.0.1')) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+  optionsSuccessStatus: 200,
+};
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const routes = Router();
 
 app.get('/', async (_, res: Response) => {
-  res.json({"message":'Server Working Just Fine'});
-}
-);
+  res.json({ message: 'Server Working Just Fine' });
+});
 
-routes.use('/items', items )
-routes.use('/people', people )
+routes.use('/items', items);
+routes.use('/people', people);
 
 app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => {
-console.log(`🔥 Server Running on Port: ${process.env.PORT} 🔥`);
+  console.log(`🔥 Server Running on Port: ${process.env.PORT} 🔥`);
 });
